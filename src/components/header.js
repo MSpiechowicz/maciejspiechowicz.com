41e3d15cd7d969e3f4153fa0912eef5bdc57eac3
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2021-05-18 11:37:04 +0200

    Revamp of page scss

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,47 @@
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react"
+import { Home, HelpCircle, Mail } from "react-feather"
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <nav>
-      <StaticImage
-        src="../images/signature-full.png"
-        className="image-signature-full"
-        quality={95}
-        alt="Signature"
-      />
-      <div className="nav-links">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact</Link>
+export default function Header() {
+  return (
+    <header className="background__color--secondary">
+      <div className="container text--center">
+        <div className="header-name">
+          <h1>Maciej Spiechowicz</h1>
+        </div>
+        <div className="header-title">
+          <h3>Project Manager & Software Engineer</h3>
+        </div>
+        <div className="header-menu">
+          <input type="checkbox" class="header-menu__button--activate"></input>
+          <div className="header-menu__hamburger">
+            <div></div>
+          </div>
+          <div className="header-menu__navigation background__color--secondary">
+            <div className="header-menu__navigation__item">
+              <a href="/" alt="Home" className="text__decoration--none">
+                <Home />
+                Home
+              </a>
+            </div>
+            <div className="header-menu__navigation__item">
+              <a href="/about" alt="About" className="text__decoration--none">
+                <HelpCircle />
+                About
+              </a>
+            </div>
+            <div className="header-menu__navigation__item">
+              <a
+                href="/contact"
+                alt="Contact"
+                className="text__decoration--none"
+              >
+                <Mail />
+                Contact
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </nav>
-  </header>
-)
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
+    </header>
+  )
 }
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
-
-export default Header

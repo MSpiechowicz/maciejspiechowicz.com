bb0f0bae332020ad6d06dbafe09b3d34ef607e2b
Maciej Spiechowicz <maciej.spiechowicz@gmail.com>
2021-05-21 14:23:13 +0200

    Adding css grid to the header

--- src/components/header/PageHeaderContent.js
+++ src/components/header/PageHeaderContent.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const PageHeaderContent = () => {
   return (
-    <div className="page-header__content">
+    <div className="page-header__content text--center">
       <h1>Maciej Spiechowicz</h1>
       <h3>Project Manager & Software Engineer</h3>
     </div>

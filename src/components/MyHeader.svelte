8aab7b0e1e3a78c9a54390f4ba6c00981850a58a
Maciej Spiechowicz <spm1fr@bosch.com>
2024-01-08 12:43:27 +0100

    moving to svelte

---
+++ src/components/MyHeader.svelte
@@ -0,0 +1,20 @@
+<header>
+  <div class="logo-container">
+    <svg xmlns="http://www.w3.org/2000/svg" width="228" height="266" fill="none" viewBox="0 0 228 266">
+      <g class="Group 3">
+        <path stroke="#000" stroke-linecap="round" stroke-width="8" d="M223.578 223.125S173-86.5 115 79.5 72.988-69.341 26.988 20.659 21.195 232.46 21.195 232.46" class="Vector 1"/>
+        <path stroke="#000" stroke-linecap="round" stroke-width="8" d="M192 225.5s-8.734-47.998-16.5-81.5c-9.963-42.977-20.014-71.992-37.5-33-31.117 69.387-76.067 77.02-89.5 26C35.067 85.98 32 67 32 67" class="Vector 2"/>
+      </g>
+    </svg>
+    <span>Maciej Spiechowicz</span>
+  </div>
+  <nav>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+    <a href="#blog">Blog</a>
+  </nav>
+</header>
+
+<style lang="scss">
+  @import '../styles/header.scss';
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Header from '../components/MyHeader.svelte';
+</script>
+
+<div class="main-container">
+  <Header />
+  <main></main>
+  <footer></footer>
+</div>
+
+<style lang="scss">
+  @import '../styles/font.scss';
+  @import '../styles/global.scss';
+	@import '../styles/reset.scss';
+
+  .main-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 100vw;
+    height: 100vh;
+  }
+</style>
